41fd7fea7b3a87a0ac01c06eb1dcca2d7d0abea6
Randy Pitcher <randypitcherii@gmail.com>
2018-01-10 14:59:32 -0600

    implemented rudimentary getting and posting of models

--- monocker-registry/monocker-registry/app.js
+++ monocker-registry/monocker-registry/app.js
@@ -17,7 +17,5 @@ SwaggerExpress.create(config, function(err, swaggerExpress) {
   var port = 10010;
   app.listen(port);
 
-  if (swaggerExpress.runner.swagger.paths['/hello']) {
-    console.log('try this:\ncurl http://127.0.0.1:' + port + '/hello?name=Scott');
-  }
+  console.log('listening on port ' + port);   
 });
--- monocker-registry/runContainer.sh
+++ monocker-registry/runContainer.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -d -p 10010:10010 randypitcherii/monocker-registry 
+docker run --rm -d -p 10010:10010 -p 81:81 ${USER}/monocker-registry 
