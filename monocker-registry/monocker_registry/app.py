186ea5d010c7b51f2a379447440f9fac0c99068c
Randy Pitcher <randypitcherii@gmail.com>
2018-01-17 12:25:11 -0600

    set debug flag to False in registry app.py

--- monocker-registry/monocker_registry/app.py
+++ monocker-registry/monocker_registry/app.py
@@ -18,5 +18,5 @@ app.register_blueprint(blueprint)
 
 # Run app
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', debug=False)
 #==============================================================================
